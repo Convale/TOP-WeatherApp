fe4c0559e2a4a0f7ef11055f1346bd275997aa36
Shaun Payne <convale@gmail.com>
2020-06-30 20:32:58 -0700

    Updates

---
+++ precache-manifest.9963726c9d99bad0ee704729547ab001.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8104c80c230f68b0de03768b2d0f1b2",
+    "url": "/TOP-WeatherApp/index.html"
+  },
+  {
+    "revision": "5a4393c07a9f3731c5e3",
+    "url": "/TOP-WeatherApp/static/css/main.1029af49.chunk.css"
+  },
+  {
+    "revision": "0b0162572d7fecb3ea03",
+    "url": "/TOP-WeatherApp/static/js/2.daa7aa22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TOP-WeatherApp/static/js/2.daa7aa22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4393c07a9f3731c5e3",
+    "url": "/TOP-WeatherApp/static/js/main.198ff17e.chunk.js"
+  },
+  {
+    "revision": "274c8b01b517802afef2",
+    "url": "/TOP-WeatherApp/static/js/runtime-main.63bca75e.js"
+  },
+  {
+    "revision": "3eec3fdf1005a79a80ec30fcef7102a7",
+    "url": "/TOP-WeatherApp/static/media/icon_01d.3eec3fdf.png"
+  },
+  {
+    "revision": "bdd4f32632e1f2d7231358c45aec396f",
+    "url": "/TOP-WeatherApp/static/media/icon_01n.bdd4f326.png"
+  },
+  {
+    "revision": "54d939161cb3389f113363f14d7d63ac",
+    "url": "/TOP-WeatherApp/static/media/icon_02d.54d93916.png"
+  },
+  {
+    "revision": "2608e72901d769c3b8160169e0ead853",
+    "url": "/TOP-WeatherApp/static/media/icon_02n.2608e729.png"
+  },
+  {
+    "revision": "3ff7877948511456f9ddabaa54dbf607",
+    "url": "/TOP-WeatherApp/static/media/icon_03d.3ff78779.png"
+  },
+  {
+    "revision": "f9c5bca854074bfca6fd8c266ce6474b",
+    "url": "/TOP-WeatherApp/static/media/icon_03n.f9c5bca8.png"
+  },
+  {
+    "revision": "3ff7877948511456f9ddabaa54dbf607",
+    "url": "/TOP-WeatherApp/static/media/icon_04d.3ff78779.png"
+  },
+  {
+    "revision": "f9c5bca854074bfca6fd8c266ce6474b",
+    "url": "/TOP-WeatherApp/static/media/icon_04n.f9c5bca8.png"
+  },
+  {
+    "revision": "aff873caa1cff73f498ae491580f71d5",
+    "url": "/TOP-WeatherApp/static/media/icon_09d.aff873ca.png"
+  },
+  {
+    "revision": "974a616a22ef946e62d3163b85fd4328",
+    "url": "/TOP-WeatherApp/static/media/icon_09n.974a616a.png"
+  },
+  {
+    "revision": "aff873caa1cff73f498ae491580f71d5",
+    "url": "/TOP-WeatherApp/static/media/icon_10d.aff873ca.png"
+  },
+  {
+    "revision": "974a616a22ef946e62d3163b85fd4328",
+    "url": "/TOP-WeatherApp/static/media/icon_10n.974a616a.png"
+  },
+  {
+    "revision": "75a1db2b6bce546f120f48811649cb08",
+    "url": "/TOP-WeatherApp/static/media/icon_11d.75a1db2b.png"
+  },
+  {
+    "revision": "e7eecdda4160c5f91a9bf1bb7fe542b6",
+    "url": "/TOP-WeatherApp/static/media/icon_11n.e7eecdda.png"
+  },
+  {
+    "revision": "03182491682c9fc115c71a686aaa0dc5",
+    "url": "/TOP-WeatherApp/static/media/icon_13d.03182491.png"
+  },
+  {
+    "revision": "b9bd6540a22afe1d40b612347f86a35d",
+    "url": "/TOP-WeatherApp/static/media/icon_13n.b9bd6540.png"
+  },
+  {
+    "revision": "e854bb6103b46fe80df1947991aca3ea",
+    "url": "/TOP-WeatherApp/static/media/icon_50d.e854bb61.png"
+  },
+  {
+    "revision": "6352df72cdfd5b3932c738d496a0efc4",
+    "url": "/TOP-WeatherApp/static/media/widget_background.6352df72.png"
+  }
+]);
